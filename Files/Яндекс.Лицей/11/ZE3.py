707eacde684bd833e5059832a09a3359a4745747
Mac <tkondrashin@icloud.com>
2020-01-07 18:48:52 +0300

    add

--- Files/Яндекс.Лицей/11/ZE3.py
+++ Files/Яндекс.Лицей/11/ZE3.py
@@ -1,24 +1,17 @@
-a = -1
-value_l = 0
-value_h = 0
-s = 0
-while a != 0:
-    a = int(input())
-    if value_l == 0:
-        value_l = a
-    if a != 0:
-        if a > value_h:
-            s += 1
-            if a > value_h:
-                value_h = a
-                s += 1
-            else:
-                s = 0
-            if a < value_l:
-                value_l = a
-        else:
-            s = 0
-
-    else:
-        break
-print(value_l, value_h)
+x1 = int(input())
+x2 = int(input())
+xl = 0
+xh = 0
+flag = False
+while x2 != 0:
+    if flag is False:
+        if x1 < x2:
+            xl = x2
+            flag = True
+    if flag:
+        if x1 > x2:
+            xh = x2
+            break
+    x1 = x2
+    x2 = int(input())
+print(xl, xh, xh - xl)
--- Files/Яндекс.Лицей/9/ZE3.py
+++ Files/Яндекс.Лицей/9/ZE3.py
@@ -1,29 +1,100 @@
-p1 = int(input())
-p2 = int(input())
-p3 = int(input())
-z1s = ""
-z2s = ""
-z3s = ""
-n = 0
-while p1 != 0:
-    p1 //= 2
-    z1s += str(p1 % 2)
-print(z1s)
-while p2 != 0:
-    p2 //= 2
-    z2s += str(p2 % 2)
-print(z2s)
-while p3 != 0:
-    p3 //= 2
-    z3s += str(p3 % 2)
-print(z3s)
-
-s = int(z1s) + int(z2s) + int(z3s)
-sl = len(str(s))
-print(s, sl)
-while n != (sl):
-    sn = str(s)[n]
-    n += 1
-    if int(sn) % 2 != 0:
-        print(sn, n)
-        continue
+x1 = int(input(""))
+x2 = int(input(""))
+x3 = int(input(""))
+step = 0
+n = x1 ^ x2 ^ x3
+if n > 0:
+    if x1 ^ n < x1:
+        s = x1 - (x1 ^ n)
+        x1 -= s
+        ng = x1
+        xn = 1
+    elif x2 ^ n < x2:
+        s = x2 - (x2 ^ n)
+        x2 -= s
+        ng = x2
+        xn = 2
+    else:
+        s = x3 - (x3 ^ n)
+        x3 -= s
+        ng = x3
+        xn = 3
+else:
+    x1 -= 1
+    s = 1
+    xn = 1
+    ng = x1
+print("ИИ забрал:", str(s))
+print("Из кучи:", str(xn))
+print("В этой куче осталось:", str(ng), "камня(ей).")
+while x1 + x2 + x3 > 0:
+    step = 2
+    xn = 0
+    while xn < 1 or xn > 3:
+        xn = int(input("Из какой кучи взять? "))
+        if xn == 1 and x1 == 0 or xn == 2 and x2 == 0 or xn == 3 and x3 == 0:
+            print("В этой куче нет камней")
+            xn = 0
+    con = True
+    while con:
+        if xn == 1:
+            ng = x1
+        elif xn == 2:
+            ng = x2
+        else:
+            ng = x3
+        s = int(input("Сколько взять(Не больше" + str(ng) + ")?"))
+        if s > 0 and s <= ng:
+            if xn == 1:
+                x1 -= s
+                ng = x1
+            elif xn == 2:
+                x2 -= s
+                ng = x2
+            else:
+                x3 -= s
+                ng = x3
+            print("ИИ забрал:", str(s))
+            print("Из кучи:", str(xn))
+            print("В этой куче осталось:", str(ng), "камня(ей).")
+            con = False
+    if x1 + x2 + x3 > 0:
+        step = 1
+        n = x1 ^ x2 ^ x3
+        if n > 0:
+            if x1 ^ n < x1:
+                s = x1 - (x1 ^ n)
+                x1 -= s
+                ng = x1
+                xn = 1
+            elif x2 ^ n < x2:
+                s = x2 - (x2 ^ n)
+                x2 -= s
+                ng = x2
+                xn = 2
+            else:
+                s = x3 - (x3 ^ n)
+                x3 -= s
+                ng = x3
+                xn = 3
+        else:
+            if x1 > 0:
+                x1 -= 1
+                xn = 1
+                ng = x1
+            elif x2 > 0:
+                x2 -= 1
+                xn = 2
+                ng = x2
+            else:
+                x3 -= 1
+                xn = 3
+                ng = x3
+            s = 1
+        print("ИИ забрал:", str(s))
+        print("Из кучи:", str(xn))
+        print("В этой куче осталось:", str(ng), "камня(ей).")
+if step == 1:
+    print("Победил ИИ")
+else:
+    print("Победил Игрок")
